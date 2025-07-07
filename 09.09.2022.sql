60edb9336ff1654949c8d7699ddf4130d9d6dbfb
Tarik Kišija <tarik.kisija@hotmail.com>
2025-07-07 23:45:59 +0200

    Update 09.09.2022.sql

--- 09.09.2022.sql
+++ 09.09.2022.sql
@@ -6,11 +6,11 @@ go
 use  IB230000
 --2 U kreiranoj bazi podataka kreirati tabele slijedecom strukturom
 --a)	Uposlenici
---•	UposlenikID, 9 karaktera fiksne duzine i primarni kljuc,
---•	Ime 20 karaktera obavezan unos,
---•	Prezime 20 karaktera obavezan unos
---•	DatumZaposlenja polje za unos datuma i vremena obavezan unos
---•	Opis posla 50 karaktera obavezan unos
+--â€¢	UposlenikID, 9 karaktera fiksne duzine i primarni kljuc,
+--â€¢	Ime 20 karaktera obavezan unos,
+--â€¢	Prezime 20 karaktera obavezan unos
+--â€¢	DatumZaposlenja polje za unos datuma i vremena obavezan unos
+--â€¢	Opis posla 50 karaktera obavezan unos
 
 create table Uposlenici
 (
@@ -22,13 +22,13 @@ Opis  varchar(50)
 )
 
 --b)	Naslovi
---•	NaslovID 6 karaktera primarni kljuc,
---•	Naslov 80 karaktera obavezan unos,
---•	Tip 12 karaktera fiksne duzine obavezan unos
---•	Cijena novcani tip podatka,
---•	NazivIzdavaca 40 karaktera,
---•	GradIzdavaca 20 karaktera,
---•	DrzavaIzdavaca 30 karaktera
+--â€¢	NaslovID 6 karaktera primarni kljuc,
+--â€¢	Naslov 80 karaktera obavezan unos,
+--â€¢	Tip 12 karaktera fiksne duzine obavezan unos
+--â€¢	Cijena novcani tip podatka,
+--â€¢	NazivIzdavaca 40 karaktera,
+--â€¢	GradIzdavaca 20 karaktera,
+--â€¢	DrzavaIzdavaca 30 karaktera
 
 create table Naslovi
 (
@@ -45,11 +45,11 @@ create table Naslovi
 
 
 --c)	Prodaja
---•	ProdavnicaID 4 karktera fiksne duzine, strani i primarni kljuc
---•	Broj narudzbe 20 karaktera primarni kljuc,
---•	NaslovID 6 karaktera strani i primarni kljuc
---•	DatumNarudzbe polje za unos datuma i vremena obavezan unos
---•	Kolicina skraceni cjelobrojni tip obavezan unos
+--â€¢	ProdavnicaID 4 karktera fiksne duzine, strani i primarni kljuc
+--â€¢	Broj narudzbe 20 karaktera primarni kljuc,
+--â€¢	NaslovID 6 karaktera strani i primarni kljuc
+--â€¢	DatumNarudzbe polje za unos datuma i vremena obavezan unos
+--â€¢	Kolicina skraceni cjelobrojni tip obavezan unos
 
 create table Prodaja
 (
@@ -65,9 +65,9 @@ create table Prodaja
 
 
 --d)	Prodavnice
---•	ProdavnicaID 4 karaktera fiksne duzine primarni kljuc
---•	NazivProdavnice 40 karaktera
---•	Grad 40 karaktera
+--â€¢	ProdavnicaID 4 karaktera fiksne duzine primarni kljuc
+--â€¢	NazivProdavnice 40 karaktera
+--â€¢	Grad 40 karaktera
 create table Prodavnice
 (
 
@@ -80,11 +80,11 @@ Grad varchar(40)
 
 --3 Iz baze podataka pubs u svoju bazu prebaciti slijedece podatke
 --a)	U tabelu Uposlenici dodati sve uposlenike
---•	emp_id -> UposlenikID
---•	fname -> Ime
---•	lname -> Prezime
---•	hire_date - > DatumZaposlenja
---•	job_desc - > Opis posla
+--â€¢	emp_id -> UposlenikID
+--â€¢	fname -> Ime
+--â€¢	lname -> Prezime
+--â€¢	hire_date - > DatumZaposlenja
+--â€¢	job_desc - > Opis posla
 
 insert into Uposlenici(UposlenikID,Ime,Prezime,	DatumZaposlenja,Opis)
 selecT e.emp_id,e.fname,e.lname,e.hire_date,j.job_desc
@@ -94,13 +94,13 @@ select* from Uposlenici
 
 
 --b)	U tabelu naslovi dodati sve naslove, na mjestu gdje nema pohranjenih podataka o --nazivima izdavaca zamijeniti vrijednost sa nepoznat izdavac
---•	Title_id -> NaslovID
---•	Title->Naslov
---•	Type->Tip
---•	Price->Cijena
---•	Pub_name->NazivIzdavaca
---•	City->GradIzdavaca
---•	Country-DrzavaIzdavaca
+--â€¢	Title_id -> NaslovID
+--â€¢	Title->Naslov
+--â€¢	Type->Tip
+--â€¢	Price->Cijena
+--â€¢	Pub_name->NazivIzdavaca
+--â€¢	City->GradIzdavaca
+--â€¢	Country-DrzavaIzdavaca
 
 insert into Naslovi(NaslovID,Naslov,Tip,Cijena,NazivIzdavaca,GradIzdavaca,DrzavaIzdavaca)
 select t.title_id,t.title,t.type,t.price,ISNULL(p.pub_name, 'nepoznat izdavac'),p.city,p.country
@@ -108,20 +108,20 @@ from pubs.dbo.titles as t left join pubs.dbo.publishers as p on p.pub_id=t.pub_i
 
 
 --c)	U tabelu prodaja dodati sve stavke iz tabele prodaja
---•	Stor_id->ProdavnicaID
---•	Order_num->BrojNarudzbe
---•	titleID->NaslovID,
---•	ord_date->DatumNarudzbe
---•	qty->Kolicinaproda
+--â€¢	Stor_id->ProdavnicaID
+--â€¢	Order_num->BrojNarudzbe
+--â€¢	titleID->NaslovID,
+--â€¢	ord_date->DatumNarudzbe
+--â€¢	qty->Kolicinaproda
 
 insert into Prodaja(ProdavnicaID,BrojNaruzdbe,NaslovID,DatumNaruzdbe,Kolicina)
 selecT s.stor_id,s.ord_num,s.title_id,s.ord_date,s.qty
 from pubs.dbo.sales as s
 
 --d)	U tabelu prodavnice dodati sve prodavnice
---•	Stor_id->prodavnicaID
---•	Store_name->NazivProdavnice
---•	City->Grad
+--â€¢	Stor_id->prodavnicaID
+--â€¢	Store_name->NazivProdavnice
+--â€¢	City->Grad
 
 insert into Prodavnice(ProdavnicaId,NazivProdavnice,Grad)
 selecT s.stor_id,s.stor_name,s.city
@@ -146,11 +146,11 @@ exec sp_delete_uposlenik @UposlenikID='Y-L77953M'
 
 --b)	Kreirati tabelu Uposlenici_log slijedeca struktura
 --Uposlenici_log
---•	UposlenikID 9 karaktera fiksne duzine
---•	Ime 20 karaktera
---•	Prezime 20 karakera,
---•	DatumZaposlenja polje za unos datuma i vremena
---•	Opis posla 50 karaktera
+--â€¢	UposlenikID 9 karaktera fiksne duzine
+--â€¢	Ime 20 karaktera
+--â€¢	Prezime 20 karakera,
+--â€¢	DatumZaposlenja polje za unos datuma i vremena
+--â€¢	Opis posla 50 karaktera
 
 create table Uposlenici_log
 (
@@ -181,8 +181,7 @@ exec sp_delete_uposlenik 'A-R89858F'
 
 
 --d)	Prikazati sve uposlenike zenskog pola koji imaju vise od 10 godina radnog -staza,- a rade na Production ili Marketing odjelu. Upitom je potrebno pokazati -spojeno -ime i prezime uposlenika, godine radnog staza, te odjel na kojem rade -uposlenici. -Rezultate upita sortirati u rastucem redoslijedu prema nazivu odjela,- te -opadajucem prema godinama radnog staza (AdventureWorks2019)
---prikazti:spojeno imeprezime,godine staza,odjel na kojem rade
---uslovi:zenski pol,imaju vise od 10g radnog staza a rade na produciton ili marketing
+
 
 select CONCAT(p.FirstName,' ',p.LastName)as'imeprezime',DATEDIFF(YEAR,e.HireDate,GETDATE())as 'godine staza',
       d.Name as 'odjel'
@@ -195,9 +194,7 @@ order by 3 asc,2 desc
 
 
 --e)	Kreirati upit kojim ce se prikazati koliko ukupno je naruceno komada proizvoda --za svaku narudzbu pojedinacno, te ukupnu vrijednost narudzbe sa i bez popusta. --Uzwti u obzir samo one narudzbe kojima je datum narudzbe do datuma isporuke --proteklo manje od 7 dana (ukljuciti granicnu vrijednost), a isporucene su kupcima --koji zive na podrucju Madrida, Minhena,Seatle. Rezultate upita sortirati po broju- -komada u opadajucem redoslijedu, a vrijednost je potrebno zaokruziti na dvije --decimale (Northwind)
---prikazati:ukupno naruceno proizvoda,ukupna vrijednost sa i bez popusta POJEDINACNO
---uslov:od datuma narudzbe do isporuke manje od 7 dana ili jednako a isporucene kupcima u madridu minehu i seattlu
---sort by broj komada desc 
+
 
 select o.OrderID,SUM(od.Quantity)as' ukupno naruceno',round(SUM(od.Quantity*od.UnitPrice),2)as 'bez popusta',
       round(SUM(od.Quantity*od.UnitPrice*(1-od.Discount)),2)as 'sa popustom'
@@ -209,13 +206,13 @@ order by 2 desc
 
 --f)	Napisati upit kojim ce se prikazati brojNarudzbe,datumNarudzbe i sifra. --Prikazati samo one zapise iz tabele Prodaja ciji broj narudzbe ISKLJICIVO POCINJE --jednim slovom, ili zavrsava jednim slovom (Novokreirana baza)
 --Sifra se sastoji od slijedecih vrijednosti:
---•	Brojevi (samo brojevi) uzeti iz broja narudzbi,
---•	Karakter /
---•	Zadnja dva karaktera godine narudbze /
---•	Karakter /
---•	Id naslova
---•	Karakter /
---•	Id prodavnice
+--â€¢	Brojevi (samo brojevi) uzeti iz broja narudzbi,
+--â€¢	Karakter /
+--â€¢	Zadnja dva karaktera godine narudbze /
+--â€¢	Karakter /
+--â€¢	Id naslova
+--â€¢	Karakter /
+--â€¢	Id prodavnice
 --Za broj narudzbe 772a sifra je 722/19/PS2091/6380
 --Za broj narudzbe N914008 sifra je 914008/19/PS2901/6380
 
@@ -235,8 +232,6 @@ where p.BrojNaruzdbe like '[A-Za-z]%' or p.BrojNaruzdbe like '%[A-Za-z]'
 
 --5
 --a)	Prikazati nazive odjela gdje radi najmanje odnosno najvise uposlenika --(AdventureWorks2019)
---prikazati: nazive odjela i broj radnika
---uslov:najamnje i najvise ranika
 
 select*
 from(
@@ -264,8 +259,7 @@ order by 2 asc
 
 
 --b)	Prikazati spojeno ime i prezime osobe,spol, ukupnu vrijednost redovnih bruto --prihoda, ukupnu vrijednost vandrednih prihoda, te sumu ukupnih vandrednih prihoda --i ukupnih redovnih prihoda. Uslov je da dolaze iz Latvije, Kine ili Indonezije a --poslodavac kod kojeg rade je registrovan kao javno ustanova (Prihodi)
---prikazati:spojeno imeprezime, ukupnu vrijednosrt redobvrnih bruto ukupnu vandredin te sumu ukupnih vanrednih i ukupnih redovnih
---uslov: dolaze iz latvije kine indonezije a poslavac je ajvna ustanova
+
 
 selecT CONCAT(o.Ime,' ',o.PrezIme),o.spol,SUM(RP.Bruto)as 'ukupno bruto',SUM(vp.IznosVanrednogPrihoda)as 'ukupno vandredin',
        SUM(RP.Bruto+vp.IznosVanrednogPrihoda)as 'ukupno'
